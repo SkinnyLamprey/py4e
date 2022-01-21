7a25c2fb1ec50bbcc5b9549bcfd08748159fb9ec
Skinny_Lamprey <skinny_lamprey@hotmail.com>
2022-01-21 06:41:56 +0000

    It works! ex_13_03
    Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL, (2) displaying up to 3000 characters, and (3) counting the overall number of characters in the document. Don’t worry about the headers for this exercise, simply show the first 3000 characters of the document contents.

--- ex_13_03/ex_13_03.py
+++ ex_13_03/ex_13_03.py
@@ -3,20 +3,26 @@ import urllib.request
 
 char_count = 0
 printed_chars = 0
+flag = True
+stop_printing = 3000
 
 url = input('Enter link: ')
 if url == '' : url = 'http://data.pr4e.org/romeo.txt'
 fhand = urllib.request.urlopen(url)
 for line in fhand:
-    print(line)
-    for char in line.decode().strip():
-        if char != '\n' :
-            print(char, end='')
-    # char_count += len(line)
-    # if char_count < 3000 :
-    #     print(line.decode().strip())
-    #     printed_chars += len(line)
-    # else :
-    #     continue
-print(printed_chars)
+    display = line.decode().strip()
+    char_count += len(line)
+    if char_count < stop_printing : 
+        print(display)
+        printed_chars += len(display)
+        len_of_stop = stop_printing - printed_chars
+        continue
+    elif flag :
+        print(display[:len_of_stop])
+        printed_chars += len(display[:len_of_stop])
+        flag = False
+    # print('rem', char_count)
+    # print('flag', len(line))
+    
+print(f'\nNumber of printed characters is {printed_chars}')
 print(f'There are {char_count} characters in {url}')
