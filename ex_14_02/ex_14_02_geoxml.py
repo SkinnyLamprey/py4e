324a808be1004d7c54afb3c242c8a3675a3bc773
Skinny_Lamprey <skinny_lamprey@hotmail.com>
2022-01-25 07:25:29 +0000

    Update ex_14_02_geoxml.py
    Finished exercise.

--- ex_14_02/ex_14_02_geoxml.py
+++ ex_14_02/ex_14_02_geoxml.py
@@ -2,17 +2,6 @@ import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
-# api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
-# if api_key is False:
-#     api_key = 42
-#     serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-# else :
-#     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -21,35 +10,16 @@ ctx.verify_mode = ssl.CERT_NONE
 while True:
     url = input('Enter URL: ')
     if len(url) < 1: break
-
-    # parms = dict()
-    # parms['url'] = url
-    # if api_key is not False: parms['key'] = api_key
-    # url = serviceurl + urllib.parse.urlencode(parms)
     print('Retrieving', url)
     uh = urllib.request.urlopen(url, context=ctx)
 
     data = uh.read()
     print('Retrieved', len(data), 'characters')
-    #print(data.decode())
     tree = ET.fromstring(data)
 
     counts = tree.findall('.//count')
-    #print('\nCOUNTS', counts)
     total = 0
     for num in counts :
-        #print(num.text)
         num = int(num.text)
         total += num
     print(total)
-
-    # results = tree.findall('result')
-    # num = counts[0].find('comment').find('count').text
-    # lng = results[0].find('geometry').find('location').find('lng').text
-    # location = results[0].find('formatted_address').text
-
-    # print('NUM', num)
-    # print(location)
-
-
-
